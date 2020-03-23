f2f3d3818f3d9afdda9664b5eefda4035cd14345
Yury Evtikhov <y@0x0a.dev>
2020-03-23 22:29:29 +0000

    Working example

--- num-server/cmd/server.go
+++ num-server/cmd/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
 	"fmt"
 	"net"
@@ -14,127 +13,108 @@ import (
 	"golang.org/x/net/netutil"
 )
 
-var ErrNetClosing = errors.New("use of closed network connection")
-
-type Server struct {
-	listener net.Listener
-	stop     chan struct{}
+type Config struct {
+	maxClients     uint
+	port           uint
+	host           string
+	log            string
+	readTimeout    time.Duration
+	reportInterval time.Duration
 }
 
-func StartServer(ctx context.Context, host string, port int, clients int) (*Server, error) {
+func ListenAndServe(conf Config) error {
+	stop := make(chan struct{})
+	defer close(stop)
+
 	// Initializing network
-	l, err := net.Listen("tcp", fmt.Sprintf("%s:%d", host, port))
+	nl, err := net.Listen("tcp", fmt.Sprintf("%s:%d", conf.host, conf.port))
 	if err != nil {
-		return nil, fmt.Errorf("unable to bind tcp socket: %w", err)
+		return fmt.Errorf("unable to bind tcp socket: %w", err)
 	}
-	listener := netutil.LimitListener(l, clients)
-	s := &Server{
-		listener: listener,
-		stop:     make(chan struct{}),
-	}
-	return s, nil
-}
+	ll := netutil.LimitListener(nl, int(conf.maxClients))
 
-func (s *Server) serve(ctx context.Context) {
-	for {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Initializing a Deduper
+	dd := deduper.New()
+	defer dd.Close()
+
+	// Create() will create or truncate the file
+	file, err := os.Create(conf.log)
+	if err != nil {
+		cancel()
+		return fmt.Errorf("cannot create log file: %w", err)
+	}
+	defer file.Close()
+
+	var wg sync.WaitGroup
+	go func() {
+		wg.Add(1)
+		dd.Log(ctx, file, conf.reportInterval)
+		wg.Done()
+	}()
+
+	go func() {
 		select {
 		case <-ctx.Done():
-			break
-		default:
+		case <-stop:
 		}
-		conn, err := s.listener.Accept()
-		if err != nil {
-			if !errors.Is(err, ErrNetClosing) {
-				fmt.Fprintf(os.Stderr, "error accepting TCP connection: %s", err.Error())
+		ll.Close()
+	}()
+
+	err = serve(ctx, dd, ll, conf.readTimeout, cancel)
+	wg.Wait()
+	return err
+}
+
+// serve accepts connections from net.Listener and passes them
+// to deduper.Deduper for futher processing.
+//
+// Connections have read timeout of time.Duration.
+// Parent context can be cancelled using context.CancelFunc.
+func serve(ctx context.Context, dd deduper.Deduper, nl net.Listener, rto time.Duration, canc context.CancelFunc) error {
+
+	var (
+		wg  sync.WaitGroup
+		err error
+	)
+	defer wg.Wait()
+
+	for {
+		conn, e := nl.Accept()
+		if e != nil {
+			return e
+		}
+		wg.Add(1)
+		go func() {
+			err := dd.FromNetwork(conn, rto)
+			if err == deduper.ErrTerminationSeq {
+				canc()
 			}
-			break
-		}
-		connCh <- conn
+			conn.Close()
+			wg.Done()
+		}()
 	}
-	return
+	return err
 }
 
 func run(args []string) error {
 
 	// Flags initialization
 	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
-	var (
-		host           = *flags.String("host", "127.0.0.1", "host")
-		port           = *flags.Int("port", 4000, "port")
-		log            = *flags.String("log", "numbers.log", "log file")
-		maxClients     = *flags.Int("max", 5, "max clients")
-		reportInterval = *flags.Duration("report", 10*time.Second, "report interval")
-		readTimeout    = *flags.Duration("readTimeout", 10*time.Second, "client read timeout")
-	)
+	cfg := Config{
+
+		host:           *flags.String("host", "127.0.0.1", "host"),
+		port:           *flags.Uint("port", 4000, "port"),
+		log:            *flags.String("log", "numbers.log", "log file"),
+		maxClients:     *flags.Uint("max", 5, "max clients"),
+		reportInterval: *flags.Duration("report", 10*time.Second, "report interval"),
+		readTimeout:    *flags.Duration("readTimeout", 10*time.Second, "client read timeout"),
+	}
 	if err := flags.Parse(args[1:]); err != nil {
 		return err
 	}
-
-	// Create() will create or truncate the file
-	file, err := os.Create(log)
-	if err != nil {
-		return fmt.Errorf("cannot create log file: %w", err)
-	}
-	defer file.Close()
-
-	ctx, cancel := context.WithCancel(context.Background())
-
-	// Initialize a deduper
-	dd := deduper.New()
-	defer dd.Close()
-
-	// Start writing log and reporting to console
-	go dd.Log(ctx, file, reportInterval)
-
-	var (
-		wg     sync.WaitGroup
-		runErr error
-	)
-	// Handling connections until context cancelled
-	connCh := make(chan net.Conn, 1)
-
-	go func(ctx context.Context) {
-		for {
-			select {
-			case <-ctx.Done():
-				break
-			default:
-			}
-			println("new connection waiting")
-			conn, err := listener.Accept()
-			if err != nil {
-				if !errors.Is(err, ErrNetClosing) {
-					runErr = fmt.Errorf("error accepting TCP connection: %w", err)
-				}
-				println("aborted new connection")
-				break
-			}
-			println("connection accepted")
-			connCh <- conn
-		}
-		println("connection dispatcher returning")
-		return
-	}(ctx)
-
-	for conn := range connCh {
-		wg.Add(1)
-		go func() {
-			conn.SetReadDeadline(time.Now().Add(readTimeout))
-
-			err := dd.FromNetwork(conn, readTimeout)
-			if err == deduper.ErrTerminationSeq {
-				cancel()
-				close(connCh)
-				println("conn channel closed")
-			}
-			conn.Close()
-			wg.Done()
-			return
-		}()
-	}
-	println("waiting for all goroutines to complete")
-	wg.Wait()
-	return runErr
+	return ListenAndServe(cfg)
 }
 
 func main() {
--- num-server/deduper/deduper.go
+++ num-server/deduper/deduper.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"bytes"
 	"context"
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
@@ -25,13 +24,20 @@ var (
 	ErrTerminationSeq  = errors.New("termination sequence received")
 )
 
+// payload is used to transmit the number alongside
+// its []byte representation to the Log() goroutine.
+type payload struct {
+	number uint32
+	bytes  []byte
+}
+
 // Deduper receives slices of bytes as messsages
 // and after processing writes those to
 // a deduplicated stream.
 type Deduper struct {
 	// input receives vetted slices for
 	// further processing
-	input chan []byte
+	input chan payload
 
 	// seen is a lookup map to deduplicate
 	// values
@@ -45,7 +51,7 @@ type Deduper struct {
 // New returns an initialized Deduper.
 func New() Deduper {
 	return Deduper{
-		input: make(chan []byte, 1),
+		input: make(chan payload),
 		seen:  make(map[uint32]struct{}),
 	}
 }
@@ -75,17 +81,24 @@ func (d Deduper) FromNetwork(nc net.Conn, readTimeout time.Duration) error {
 		nc.SetReadDeadline(time.Now().Add(readTimeout))
 
 		data := bytes.TrimSpace(byteData)
+		fmt.Println("Received data:", data)
 		if bytes.Compare(data, []byte(terminator)) == 0 {
 			err = ErrTerminationSeq
 			break
 		}
 
-		if len(data) != expectedBytes || !bytesAreUint(data) {
+		if len(data) != expectedBytes {
 			err = ErrUnexpectedInput
 			break
 		}
+		var num uint32
+		if err = bytesToUint32(data, &num); err != nil {
+			break
+		}
 
-		d.input <- data
+		// Both number and byte data sent down the input channel
+		// to avoid reverse conversion from uint32 to []byte.
+		d.input <- payload{num, data}
 	}
 	return err
 }
@@ -105,29 +118,32 @@ func (d *Deduper) Log(ctx context.Context, w io.Writer, reportInterval time.Dura
 	// Buffered writes can help performance during the Write() operations
 	// by buffering 4096 bytes before writing.
 	bw := bufio.NewWriter(w)
+	defer func() {
+		println("Flushing file")
+		bw.Flush()
+	}()
 
+	ticker := time.NewTicker(reportInterval)
+	defer ticker.Stop()
 	for {
 		select {
 		case data := <-d.input:
-
-			println("Data:", data)
-			number := binary.BigEndian.Uint32(data)
-			println("Number:", number)
-			if d.lookup(number) {
+			fmt.Printf("Number: %d\n", data.number)
+			if d.lookup(data.number) {
 				dups.inc()
-				continue
+				break
 			}
-
-			d.record(number)
+			d.record(data.number)
 			uniq.inc()
 
-			_, d.err = bw.Write(append(data, '\n'))
+			println("Writing:", data.bytes)
+			_, d.err = bw.Write(append(data.bytes, '\n'))
 			if d.err != nil {
-				// No need to flush, can return
-				// immediately
+				println("Error writing file:", d.err)
+				// No need to flush, can return immediately
 				return
 			}
-		case <-time.Tick(reportInterval):
+		case <-ticker.C:
 			fmt.Printf(
 				"Received %d unique numbers, %d duplicates. Unique total: %d\n",
 				uniq, dups, d.count(),
@@ -135,10 +151,9 @@ func (d *Deduper) Log(ctx context.Context, w io.Writer, reportInterval time.Dura
 			uniq.reset()
 			dups.reset()
 		case <-ctx.Done():
-			break
+			return
 		}
 	}
-	d.err = bw.Flush()
 	return
 }
 
@@ -178,11 +193,19 @@ func (c *counter) inc() { *c++ }
 // reset resets the counter back to 0.
 func (c *counter) reset() { *c = 0 }
 
-func bytesAreUint(b []byte) bool {
-	for i := 0; i < len(b); i++ {
-		if b[i] < 0x30 || b[i] > 0x39 {
-			return false
+func bytesToUint32(b []byte, n *uint32) error {
+
+	var (
+		pos uint32 = 1
+		num uint32
+	)
+	for i := len(b) - 1; i >= 0; i-- {
+		if b[i] < '0' || b[i] > '9' {
+			return errors.New("byts are not numbers")
 		}
+		num += pos * (uint32(b[i]) - '0')
+		pos *= 10 // base 10
 	}
-	return true
+	*n = num
+	return nil
 }
